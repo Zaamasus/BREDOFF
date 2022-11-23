4734d96756b1a83336b988a81b756e0724766c01
Zaamasus <101902171+Zaamasus@users.noreply.github.com>
2022-11-22 23:00:18 -0300

    Create funcion remove show (modal)

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+ //Abre e fecha o menu quando clicar no icone: hamburer e x
+
+
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll("nav .toggle")
 
@@ -8,3 +11,12 @@ for( const element of toggle) {
   })
 }
 
+// quando clicar em um intem do menu, esconder o menu
+
+const links = document.querySelectorAll("nav ul li a ")
+
+for (const link of links) {
+  link.addEventListener('click', function(){
+    nav.classList.remove('show')
+  })
+}
