4d20c76a1c2416a7f9560b4579135f7858761c0a
Zaamasus <samuelgomes22442@gmail.com>
2022-12-02 11:48:46 -0300

    javajs

--- main.js
+++ main.js
@@ -18,10 +18,11 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
+  
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -38,7 +39,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints:{
+    767:{
+      slidesPerView: 2,
+      setWrapperSize: true,
+    }
+  }
 })
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
@@ -60,9 +67,14 @@ scrollReveal.reveal(
   { interval: 100 }
 )
 
+
+
+
 /* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
+  
 
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
@@ -71,8 +83,26 @@ function backToTop() {
   }
 }
 
+// MENU ATIVO COMNFORMA SECCAO 
+
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection(){
+
+  const checkpoint = document.pageYOffset + (window.innerHeight / 8) * 4
+
+  
+
+}
+
+
 /* When Scroll */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
+
+
+
+
+
